1c2ef6a9ebdc161ad4bef539e8cc25c43853ccb6
pranshu <singhpranshu650@gmail.com>
2020-06-27 11:20:53 +0530

    Updates

---
+++ precache-manifest.196ecefaab3fd934a90ad3affc78a032.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c48a193ceffc5cf94206716835b7f250",
+    "url": "/employeedashboard/index.html"
+  },
+  {
+    "revision": "2d20e17bf803b4a986ea",
+    "url": "/employeedashboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "abd6a8a97ba5f9cbec7b",
+    "url": "/employeedashboard/static/js/2.27beb7c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employeedashboard/static/js/2.27beb7c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d20e17bf803b4a986ea",
+    "url": "/employeedashboard/static/js/main.932f4f76.chunk.js"
+  },
+  {
+    "revision": "de0b8fe5d482fa4acd83",
+    "url": "/employeedashboard/static/js/runtime-main.a143ba31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/employeedashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
